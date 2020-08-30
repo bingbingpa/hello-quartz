025fd58b137eca1761dd65f61367ecfca38ec149
bingbingpa <bingbingpa@gmail.com>
2020-08-31 01:16:33 +0900

    quartz 설정, logback 설정 추가

---
+++ src/main/java/com/bingbingpa/config/SchedulerConfig.java
@@ -0,0 +1,56 @@
+package com.bingbingpa.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.quartz.QuartzProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@ConditionalOnProperty(name = "quartz.enabled")
+public class SchedulerConfig {
+//    @Autowired
+//    private TriggersListener triggersListener;
+//
+//    @Autowired
+//    private JobsListener jobsListener;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private QuartzProperties quartzProperties;
+
+    /**
+     * Quartz 관련 설정
+     *
+     * @param applicationContext the applicationContext
+     * @return SchedulerFactoryBean
+     */
+    @Bean
+    public SchedulerFactoryBean schedulerFactoryBean(ApplicationContext applicationContext) {
+        SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
+
+        AutowiringSpringBeanJobFactory jobFactory = new AutowiringSpringBeanJobFactory();
+        jobFactory.setApplicationContext(applicationContext);
+        schedulerFactoryBean.setJobFactory(jobFactory);
+
+        schedulerFactoryBean.setApplicationContext(applicationContext);
+
+        Properties properties = new Properties();
+        properties.putAll(quartzProperties.getProperties());
+
+//        schedulerFactoryBean.setGlobalTriggerListeners(triggersListener);
+//        schedulerFactoryBean.setGlobalJobListeners(jobsListener);
+        schedulerFactoryBean.setOverwriteExistingJobs(true);
+        schedulerFactoryBean.setDataSource(dataSource);
+        schedulerFactoryBean.setQuartzProperties(properties);
+        schedulerFactoryBean.setWaitForJobsToCompleteOnShutdown(true);
+        return schedulerFactoryBean;
+    }
+}
