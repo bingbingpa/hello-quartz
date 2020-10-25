acf14017cc55cb3ea3998340b3861a340b584198
shpark <bingbingpa@gmail.com>
2020-10-26 00:02:18 +0900

    quartz 를 공부해야 한다...

--- src/main/java/com/bingbingpa/util/ServerResponseCode.java
+++ src/main/java/com/bingbingpa/util/ServerResponseCode.java
@@ -4,11 +4,13 @@ public class ServerResponseCode {
 
     //SPECIFIC ERROR CODES
     public static final int JOB_WITH_SAME_NAME_EXIST = 501;
+
     public static final int JOB_NAME_NOT_PRESENT = 502;
 
     public static final int JOB_ALREADY_IN_RUNNING_STATE = 510;
 
     public static final int JOB_NOT_IN_PAUSED_STATE = 520;
+
     public static final int JOB_NOT_IN_RUNNING_STATE = 521;
 
     public static final int JOB_DOESNT_EXIST = 500;
