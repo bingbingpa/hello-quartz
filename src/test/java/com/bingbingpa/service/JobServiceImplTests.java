0e72fec291f96d0f75ffbb6c49eba2ef4ca63776
shpark <bingbingpa@gmail.com>
2020-10-27 00:49:26 +0900

    quartz job test

---
+++ src/test/java/com/bingbingpa/service/JobServiceImplTests.java
@@ -0,0 +1,26 @@
+package com.bingbingpa.service;
+
+import com.bingbingpa.job.CronJob;
+import com.bingbingpa.job.SimpleJob;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class JobServiceImplTests {
+
+    @Autowired
+    private JobService jobService;
+
+    @Test
+    void JobServiceExecuteTest() {
+//        jobService.scheduleOneTimeJob("testJob", CronJob.class, new Date());
+        jobService.scheduleCronJob("testJob", CronJob.class, new Date(), "1 * * * * ?");
+    }
+
+}
