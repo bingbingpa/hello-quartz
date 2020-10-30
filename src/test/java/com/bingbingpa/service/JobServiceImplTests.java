8c5c3bf2259043feff73cc73f1b69cc71e6bb8aa
shpark <bingbingpa@gmail.com>
2020-10-31 01:01:46 +0900

    불필요한 임포트 제거

--- src/test/java/com/bingbingpa/service/JobServiceImplTests.java
+++ src/test/java/com/bingbingpa/service/JobServiceImplTests.java
@@ -1,15 +1,11 @@
 package com.bingbingpa.service;
 
 import com.bingbingpa.job.CronJob;
-import com.bingbingpa.job.SimpleJob;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class JobServiceImplTests {
